5cf6fa8ad98aeb28e6b29ce64ba40c5cb67d2382
aionick <nick@nuco.io>
2019-05-27 12:12:04 -0400

    Deleted ExecutionBatch class
    - This class in not accomplishing much anymore (except for giving us an oddly-named class)
      since the transaction contexts have been removed.
    - Trying to simply the executor and reduce the number of special classes needed to
      interact with it.

--- modAionImpl/src/org/aion/zero/impl/blockchain/AionImpl.java
+++ modAionImpl/src/org/aion/zero/impl/blockchain/AionImpl.java
@@ -20,7 +20,6 @@ import org.aion.types.ByteArrayWrapper;
 import org.aion.util.bytes.ByteUtil;
 import org.aion.vm.BulkExecutor;
 import org.aion.vm.BulkExecutorBuilder;
-import org.aion.vm.ExecutionBatch;
 import org.aion.vm.exception.VMException;
 import org.aion.zero.impl.AionHub;
 import org.aion.zero.impl.config.CfgAion;
@@ -130,9 +129,8 @@ public class AionImpl implements IAionChain {
                 aionHub.getRepository().getSnapshotTo(block.getStateRoot()).startTracking();
 
         try {
-            ExecutionBatch details = new ExecutionBatch(block, Collections.singletonList(tx));
             BulkExecutor executor = new BulkExecutorBuilder()
-                .transactionBatchToExecute(details)
+                .transactionsToExecute(block, Collections.singletonList(tx))
                 .repository(repository)
                 .isLocalCall(true)
                 .allowNonceIncrement(true)
@@ -161,9 +159,8 @@ public class AionImpl implements IAionChain {
                 aionHub.getRepository().getSnapshotTo(block.getStateRoot()).startTracking();
 
         try {
-            ExecutionBatch details = new ExecutionBatch(block, Collections.singletonList(tx));
             BulkExecutor executor = new BulkExecutorBuilder()
-                .transactionBatchToExecute(details)
+                .transactionsToExecute(block, Collections.singletonList(tx))
                 .repository(repository)
                 .isLocalCall(true)
                 .allowNonceIncrement(true)
